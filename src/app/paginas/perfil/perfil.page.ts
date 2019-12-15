e3a804b5e20460b15b099808238fd05fa7c6a642
Luisa Ferreira <luisaaferreira23@gmail.com>
2019-12-15 16:49:04 -0300

    html perfil

--- src/app/paginas/perfil/perfil.page.ts
+++ src/app/paginas/perfil/perfil.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { IonSlides } from '@ionic/angular';
+import { Relato } from 'src/app/interfaces/relato';
+import { Subscription } from 'rxjs';
+import { RelatosService } from 'src/app/services/relatos.service';
 
 @Component({
   selector: 'app-perfil',
@@ -9,16 +12,28 @@ import { IonSlides } from '@ionic/angular';
 })
 export class PerfilPage implements OnInit {
   @ViewChild(IonSlides, {static: true}) slides: IonSlides
-  private usuario: Usuario = {};
 
-  constructor( ) { }
+  private relatos = new Array<Relato>();
+  private relatosSubscription: Subscription;
+
+  constructor(
+    private relatosService: RelatosService
+   ) {
+    this.relatosSubscription = this.relatosService.getRelatos().subscribe(data => {
+      this.relatos = data;
+    });
+  }
 
   ngOnInit() { }
 
+  ngOnDestroy() {
+    this.relatosSubscription.unsubscribe();
+  }
+  
   segmentChanged(event: any) {
     if (event.detail.value === 'pendentes') {
       this.slides.slidePrev();
-    } else if (event.detail.value === 'resolvidos') {
+    } else {
       this.slides.slideNext();
     }
   }
